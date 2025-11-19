89fa3a5897e8c77b8b37197fac0025e625fca219
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2025-11-19 21:10:48 +0200

    upload-new-changes

--- src/sections/Product-information.jsx
+++ src/sections/Product-information.jsx
@@ -8,6 +8,7 @@ import img4 from "../assets/4-660x880.jpg";
 import "../css/product-information.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { FaRegStar } from "react-icons/fa";
 
 function ProductInformation() {
     const { id } = useParams();
@@ -16,7 +17,7 @@ function ProductInformation() {
     const [product, setProduct] = useState({});
     const getProduct = async () => {
         try {
-            await fetch(`https://rucrudgdtmsunfhheckx.supabase.co/rest/v1/Featured-Products/?id=eq.${id}`, {
+            await fetch(`https://rucrudgdtmsunfhheckx.supabase.co/rest/v1/All-Products?id=eq.${id}`, {
                 method: "GET",
                 headers: {
                     apikey: `sb_publishable_8KKdokhVEobg4M7V12J77g__anAlcjZ`,
@@ -38,7 +39,7 @@ function ProductInformation() {
 
     return (
         <div className="w-full h-full product-information">
-            <div className="container flex w-full h-full">
+            <div className="container flex w-full h-full gap-20">
                 <div className="flex flex-col gap-5 images-content">
                     <img src={mainimg} alt="main-img-product" className="main-img-product" />
                     <div className="flex items-center justify-center gap-5 all-images-small">
@@ -51,7 +52,16 @@ function ProductInformation() {
                     </div>
                 </div>
                 <div className="information-product">
-                    <h1>information, product</h1>
+                    <div className="col-title w-full">
+                        <h1 className="text-2xl capitalize font-medium mb-2">Chainsaw tool</h1>
+                        <div className="all-stars flex gap-2">
+                            <FaRegStar />
+                            <FaRegStar />
+                            <FaRegStar />
+                            <FaRegStar />
+                            <FaRegStar />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
