f95e9e2a24767f66d6eda3789f4e4dc8c6e61d2e
Saeed Mohamed <sydalshghty15@gmail.com>
2025-12-07 15:57:05 +0200

    upload-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import Home from "./pages/Home"
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./sections/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,7 @@
-import SliderContent from "../components/slider-content";
-import BrandsSlider from "../components/brands-slider";
-import Categories from "../components/categories";
-import OurProducts from "../components/our-Products";
-//import FeaturedProducts from "../components/featured-products";
-
 function Home() {
     return (
         <>
-            <SliderContent />
-            <BrandsSlider />
-            <Categories />
-            <OurProducts />
+            <h1>home, page</h1>
         </>
     )
 }
