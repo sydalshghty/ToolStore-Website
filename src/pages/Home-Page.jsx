6a0fe80ff9636ec3356bac9ad51cc3f7067bc736
Saeed Mohamed <sydalshghty15@gmail.com>
2025-12-27 14:17:56 +0200

    upload-new-changes

--- src/pages/Home-Page.jsx
+++ src/pages/Home-Page.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/header/Header";
 import Banner from "../components/banners.jsx/banner";
+import Freedelivery from "../components/freedelivery/freedelivery";
 function HomePage() {
     return (
         <>
             <Header />
-            {/* <Banner />*/}
+            <Banner />
+            <Freedelivery />
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,10 @@ export default {
         whiteColor: "#FFFFFF",
         yellowColor: "#EBC80C",
         secondaryColor: "#1B6392",
+        gray100: "#E4E7E9",
         gray500: "#77878F",
         gray900: "#191C1F",
+        gray600: "#5F6C72",
         secondary700: "#1B6392",
         colorf6f6f6: "#f6f6f6"
       }
